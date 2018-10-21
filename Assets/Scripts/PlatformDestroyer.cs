4c9029ac2db5238b1f824606e002dc4efd6853da
littlebluelight <sayers@andrew.cmu.edu>
2018-10-21 11:54:20 -0400

    Added -background -platformGen Script -platformDegen Script -Prefabs folder with some basic prefabs -Syd

---
+++ Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+    public GameObject platformDegenerationPoint;
+
+	// Use this for initialization
+	void Start () {
+        platformDegenerationPoint = GameObject.Find("PlatformDegenerationPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position.x < platformDegenerationPoint.transform.position.x){
+
+            Destroy(gameObject);
+        }
+		
+	}
+}
